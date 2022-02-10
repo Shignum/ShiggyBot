8e2ee7812094da7f4857b7f96f5eacb64f198f45
Shignum <Danielmannig42@gmail.com>
2022-02-10 14:13:22 +0100

    test

---
+++ src/cogs/test.py
@@ -0,0 +1,12 @@
+from discord.commands import slash_command 
+from discord.ext import commands
+
+class Test(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @slash_command()
+    async def hello(self, ctx):
+        await ctx.respond("Hi, this is a slash command from a cog!")
+def setup(bot):
+    bot.add_cog(test(bot))
