868458667ebe6c0d268b0465ba73e58a31b67603
Shignum <danielmannig42@gmail.com>
2022-06-26 22:45:59 +0200

    change to pycord

--- src/bot.py
+++ src/bot.py
@@ -1,39 +1,69 @@
+import discord
+from dotenv import load_dotenv
 import os
 
-from discord import Embed
-from discord import Intents
-from discord.ext import commands
-from dotenv import load_dotenv
-
 load_dotenv()
-intents = Intents.default()
 
-bot = commands.Bot(command_prefix=os.getenv('PREFIX'))
 TOKEN = os.getenv('BOT_TOKEN')
 
-@bot.event
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
 async def on_ready():
-    print(f'{bot.user} has logged in.')
-    initial_extensions = ['cogs.event','cogs.music','cogs.other','cogs.playlist']
-    for extension in initial_extensions:
-        bot.load_extension(extension)
+    print(f'Logged on as {client.user}!')
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send(embed=Embed(title='Command not found.'))
+@client.event
+async def on_message(message):
+    if message.author == client.user:
         return
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(embed=Embed(title='Command needs an Argument.'))
-        return
-    elif isinstance(error, commands.CommandInvokeError):
-        await ctx.send(embed=Embed(title=f'{error.original}'))
-        return
-    elif isinstance(error, commands.MissingPermissions):
-        await ctx.send(embed=Embed(title="You don't have the permission to use this command."))
-        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        
 
-    raise error
+    
+client.run(TOKEN)
 
 
-bot.run(TOKEN)
+
+# from discord import Embed
+# from discord import Intents
+
+
+
+
+
+# bot = commands.Bot(command_prefix=os.getenv('PREFIX'))
+
+
+# @bot.event
+# async def on_ready():
+#     print(f'{bot.user} has logged in.')
+#     initial_extensions = ['cogs.other', 'cogs.test']
+# #    initial_extensions = ['cogs.event','cogs.music','cogs.other','cogs.playlist']
+#     for extension in initial_extensions:
+#         bot.load_extension(extension)
+#         print(f'{extension} has been loaded')
+
+# async def on_message(self, message):
+#         print(f'Message from {message.author}: {message.content}')
+
+# @bot.event
+# async def on_command_error(ctx, error):
+#     if isinstance(error, commands.CommandNotFound):
+#         await ctx.send(embed=Embed(title='Command not found.'))
+#         return
+#     elif isinstance(error, commands.MissingRequiredArgument):
+#         await ctx.send(embed=Embed(title='Command needs an Argument.'))
+#         return
+#     elif isinstance(error, commands.CommandInvokeError):
+#         await ctx.send(embed=Embed(title=f'{error.original}'))
+#         return
+#     elif isinstance(error, commands.MissingPermissions):
+#         await ctx.send(embed=Embed(title="You don't have the permission to use this command."))
+#         return
+
+#     raise error
+
